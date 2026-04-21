a1d2f8fbb74f1a2c613e3d753fe3dbe49dc8da5c
jdaubman13 <161610147+jdaubman13@users.noreply.github.com>
2026-04-21 08:47:48 -0400

    parametrize

--- test_gradebook.py
+++ test_gradebook.py
@@ -2,3 +2,8 @@ from gradebook import letter_grade
 
 def test_letter_grade_A():
     assert letter_grade(95) == 'A'
+
+def test_letter_grade_F():
+    assert letter_grade(45) == "F"
+
+@pytest.mark.parametrize("score, expected", [(95, "A"), (45, "F")])
