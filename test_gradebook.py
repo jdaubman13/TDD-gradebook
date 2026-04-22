c0684aa0252355287458271fe0c844f9aea10a41
jdaubman13 <161610147+jdaubman13@users.noreply.github.com>
2026-04-22 07:51:06 -0400

    s

--- test_gradebook.py
+++ test_gradebook.py
@@ -10,6 +10,7 @@ def test_letter_grade_F():
 def test_letter_grade(score, expected):
     assert letter_grade(score) == expected
 
-def test_letter-grade_invalid_type():
-    with pytest.raises(TypeError):
-        letter_grade("hello")
+def test_letter-grade_invalid_type(score):
+    if not isinstance()
+    #with pytest.raises(TypeError):
+        #letter_grade("hello")
